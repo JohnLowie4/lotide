c43d0a74e3470fa140736ab919e229e745fe7860
John <jzlowie@gmail.com>
2021-05-25 19:27:38 -0600

    Initial commit of eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arg1, arg2) => {
+  let bool = true;
+  for (let i = 0; i < arg1.length; i++) {
+    bool = bool && (arg1[i] === arg2[i]);
+  }
+  return bool;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
