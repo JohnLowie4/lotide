df3cdc5603c90b76185d9e1571de6cfaaaa46a04
John <jzlowie@gmail.com>
2021-05-31 15:53:04 -0600

    added test file in test/

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-const eqArrays = (arg1, arg2) => {
-  let bool = true;
-  let maxLength;
-  if (arg1.length > arg2.length) {
-    maxLength = arg1.length;
-  } else {
-    maxLength = arg2.length;
-  }
-
-  for (let i = 0; i < maxLength; i++) {
-    bool = bool && (arg1[i] === arg2[i]);
-  }
-  return bool;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   let result = eqArrays(arr1, arr2);
@@ -25,8 +12,8 @@ const assertArraysEqual = (arr1, arr2) => {
 module.exports = assertArraysEqual;
 
 // Test cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
